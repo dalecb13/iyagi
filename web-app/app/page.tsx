ffa1292a63a3bca482232ca93530aca4e6c69e67
dalecb13 <dalecb13@gmail.com>
2025-08-01 12:57:50 -0500

    added initial layout

--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -3,11 +3,11 @@ import StoryCreator from "./ui/story-creator";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 sm:items-start">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] h-screen">
+      <main className="">
         <StoryCreator />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- web-app/app/ui/story-creator.tsx
+++ web-app/app/ui/story-creator.tsx
@@ -1,14 +1,24 @@
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 
 const StoryCreator = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold mb-8">Story Creator</h1>
+    <div className="flex flex-row h-screen">
 
-      <div className="grid w-full max-w-sm items-center gap-3">
-        <Label htmlFor="storyName">Story Name</Label>
-        <Input id="storyName" type="text" />
+      {/* Left toolbar */}
+      <div className="flex-none flex justify-between bg-gray-100 border-r-gray-400 p-8">
+        <div className="grid w-full max-w-sm items-center gap-3">
+          <Input id="storyName" placeholder="Story name" type="text" />
+        </div>
+      </div>
+
+      <div className="grow flex justify-between items-center justify-cneter bg-gray-50">
+        {/* Main content */}
+        <div className="">Main content</div>
+      </div>
+
+      {/* Right toolbar */}
+      <div className="flex-none flex justify-between bg-gray-100 p-8">
+        <h1>Right toolbar</h1>
       </div>
     </div>
   )
