6f002a23bd83292f9bf635f37be027ae23b028d9
dalecb13 <dalecb13@gmail.com>
2025-08-01 12:39:42 -0500

    added shadcn and initial story creator interface

--- web-app/app/ui/story-creator.tsx
+++ web-app/app/ui/story-creator.tsx
@@ -1,10 +1,15 @@
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 const StoryCreator = () => {
   return (
     <div>
-      <h1>Story Creator</h1>
+      <h1 className="text-3xl font-bold mb-8">Story Creator</h1>
 
-      
+      <div className="grid w-full max-w-sm items-center gap-3">
+        <Label htmlFor="storyName">Story Name</Label>
+        <Input id="storyName" type="text" />
+      </div>
     </div>
   )
 }
