33eb1e1c3bc40e4a5abde77dfbd2a95b37f9f78d
dalecb13 <dalecb13@gmail.com>
2025-08-02 09:38:45 -0500

    updated page list item formatting

--- web-app/components/story-creator.tsx
+++ web-app/components/story-creator.tsx
@@ -18,6 +18,12 @@ const StoryCreator = () => {
     // if last page is not empty, add a new page
     if (pages.length > 0 && pages[pages.length - 1].contents !== "") {
       setPages([...pages, { contents: "" }]);
+      return;
+    }
+
+    if (!pages || pages.length === 0) {
+      setPages([{ contents: "" }]);
+      return;
     }
   }
 
@@ -36,21 +42,24 @@ const StoryCreator = () => {
 
         <Separator className="my-4" />
 
-        <div className="px-4 flex flex-row justify-between">
-          <h2>Pages</h2>
+        <div className="px-4 mb-4 flex flex-row justify-between">
+          <p className="font-semibold">Pages</p>
           <Button
-            variant="secondary"
+            variant="ghost"
             size="icon"
-            className="size-8"
+            className="size-6 bg-transparent hover:bg-gray-100"
             onClick={handleAddPage}
           >
             <Plus />
           </Button>
         </div>
 
-        <div className="px-4">
+        <div className="px-2">
           {pages.map((page, index) => (
-            <div key={index}>
+            <div
+              key={index}
+              className="cursor-pointer hover:bg-gray-200 py-2 px-2 rounded-md mb-2"
+            >
               <p>Page {index + 1}</p>
             </div>
           ))}
